95a86c130da7e92fdc805e765f1bae0bdee223a7
keisuke.nagaoka <keisuke19890521@gmail.com>
2023-01-21 19:10:08 +0000

    add colum

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -44,8 +44,14 @@ class TasksController extends Controller
      */
     public function store(Request $request)
     {
+        $request->validate([
+            'content' => 'required',
+            'status' => 'required|max:10',    // 追加したカラム
+        ]);
+        
         $task = new Task;
         $task->content = $request->content;
+        $task->status = $request->status;    // 追加したカラム
         $task->save();
 
         return redirect('/');
@@ -90,9 +96,15 @@ class TasksController extends Controller
      */
     public function update(Request $request, $id)
     {
+        $request->validate([
+            'content' => 'required',
+            'status' => 'required|max:10',    // 追加したカラム
+        ]);
+        
         $task = Task::findOrFail($id);
 
         $task->content = $request->content;
+        $task->status = $request->status;    // 追加したカラム
         $task->save();
 
         return redirect('/');
